6c14c3b99acecb322fbc8614c4ebe7bfb82da4c9
Carissa Ward <cward52@my.bcit.ca>
2023-12-13 15:27:39 -0800

    initial setup of identity framework

--- Novella/Data/Migrations/20231211183828_initialCreate.Designer.cs
+++ Novella/Data/Migrations/20231211183828_initialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Novella.Data;
 
@@ -11,9 +12,11 @@ using Novella.Data;
 namespace Novella.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231211183828_initialCreate")]
+    partial class initialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
