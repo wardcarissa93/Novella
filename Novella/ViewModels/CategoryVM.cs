9d29aed803f13e77129662119d3dc1fad47eb558
wanradavich <wrda.ruks@gmail.com>
2024-02-14 10:21:08 -0800

    view model cross check

--- Novella/ViewModels/CategoryVM.cs
+++ Novella/ViewModels/CategoryVM.cs
@@ -2,12 +2,14 @@
 
 namespace Novella.ViewModels
 {
-    public class Category
+    public class CategoryVM
     {
         [Key]
+        [Display(Name = "Cateogory Id")]
         public int CategoryId { get; set; }
-
+        [Display(Name = "Category Name")]
         public string CategoryName { get; set; }
+        [Display(Name = "Category Description")]
         public string CategoryDescription { get; set; }
     }
 }
