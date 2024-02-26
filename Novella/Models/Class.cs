3fab3d67dfcfded281f650937afb70eec95c6f66
akbcit <kumaraditya.ca@gmail.com>
2024-02-26 13:26:44 -0800

    corrected program.cs

---
+++ Novella/Models/Class.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace Novella.Models
+{
+    public class NullEmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            // Do nothing
+            return Task.CompletedTask;
+        }
+    }
+}
