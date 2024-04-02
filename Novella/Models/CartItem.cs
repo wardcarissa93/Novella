cebd6341e8d39efc7fe3b9ee1af518000e8e0fd4
akbcit <kumaraditya.ca@gmail.com>
2024-04-02 07:36:23 -0700

    cleared cart validation errors and cleared cart

--- Novella/Models/CartItem.cs
+++ Novella/Models/CartItem.cs
@@ -7,8 +7,8 @@
         public string ProductDescription { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
-        public string Size { get; set; }
-        public string Color { get; set; }
+        public string? Size { get; set; }
+        public string? Color { get; set; }
 
     }
 
